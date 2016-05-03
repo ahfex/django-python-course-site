a4f2591a50b5fbec200ead802b3b85d9792a6fb3
ahfex <info@impertia.com>
2016-05-03 10:58:29 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ccpfh5$sh-jk%pl@x(dz+ex!fs5&vf+$r4#ezr3_#ec&k)=&7n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
